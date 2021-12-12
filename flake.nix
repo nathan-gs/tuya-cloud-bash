3fb5602ca9383ec2ef0091fa4955a20e22653b64
Nathan Bijnens <nathan@nathan.gs>
2021-12-12 10:17:06 +0000

    Working version

--- flake.nix
+++ flake.nix
@@ -90,13 +90,8 @@
                 cat /dev/null > $F.next
                 source ${cfg.package}/bin/tuya.sh
                 source ${cfg.package}/bin/tuya_prometheus_exporter.sh
-                
-                tuya_get_token "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL"
-                accessToken=$(tuya_get_token "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL")
-                echo "accessToken: $accessToken"
-
-                echo tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key='
-                tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key='
+                                
+                accessToken=$(tuya_get_token "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL")              
                 # Batch query for the list of associated App user dimension devices
                 tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key=' | tuya_parse_batch_query > $F.next
                 
