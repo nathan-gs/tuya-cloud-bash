b31fd56d724d377816f99668b32b866b6d16346c
Nathan Bijnens <nathan@nathan.gs>
2021-12-12 09:58:06 +0000

    debug

--- flake.nix
+++ flake.nix
@@ -92,9 +92,13 @@
                 source ${cfg.package}/bin/tuya_prometheus_exporter.sh
                           
                 accessToken=$(tuya_get_token "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL")
-                
+                echo $accessToken
+
+                echo tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key='
+                tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key='
                 # Batch query for the list of associated App user dimension devices
                 tuya "$TUYA_CLOUD_CLIENTID" "$TUYA_CLOUD_SECRET" "$TUYA_CLOUD_BASEURL" $accessToken get '/v1.0/iot-01/associated-users/devices?last_row_key=' | tuya_parse_batch_query > $F.next
+                
                 mv $F.next $F
               '';
               startAt = "*:0/15";
